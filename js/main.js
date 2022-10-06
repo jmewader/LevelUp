eab9432d630b8f7df95c03c260545561361bd3f0
Настя <smelw@list.ru>
2022-10-06 23:54:43 +0300

    add new blocks

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$(".slider").slick();
+$(".slider").slick({
+  slidesToShow: 1,
+  centerMode: true,
+  dots: true,
+  adaptiveHeight: true,
+  prevArrow: "<img src='/image/svg/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='/image/svg/arrow-right.svg' class='next' alt='2'>",
+});
+
+$('.phone').on('input', function() {
+    $(this).val($(this).val().replace(/[A-Za-zА-Яа-яЁё]/, ''))
+});
