4a18abd093014f104af0fe4fa6434958dae421ae
Настя <smelw@list.ru>
2022-10-17 23:01:09 +0300

    fix urls

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(".slider").slick({
   slidesToShow: 1,
   dots: true,
   adaptiveHeight: true,
-  prevArrow: "<img src='/image/svg/arrow-left.svg' class='prev' alt='1'>",
-  nextArrow: "<img src='/image/svg/arrow-right.svg' class='next' alt='2'>",
+  prevArrow: "<img src='./image/svg/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='./image/svg/arrow-right.svg' class='next' alt='2'>",
 });
 
 $(".phone").on("input", function () {
